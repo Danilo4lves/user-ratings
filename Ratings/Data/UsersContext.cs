e3772e1c037906cc5417be9a2f27c1c4ef3023c2
Danilo Alves <danilo.alves@kinvo.com.br>
2021-02-02 11:13:32 -0300

    feat(Data): Implements UsersContext

---
+++ Ratings/Data/UsersContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ratings.Models;
+
+namespace Ratings.Data
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+    }
+}
