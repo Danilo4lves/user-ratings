247c36cf95faa25005c9d380a562631931f4290f
Danilo Alves <danilo.alves@kinvo.com.br>
2021-02-02 10:55:48 -0300

    feat(Enums): Implements Platform and RatingMood enums

---
+++ Ratings/Enums/PlatformEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Ratings.Enums
+{
+    public static class PlatformEnum
+    {
+        public static readonly string ANDROID = "ANDROID";
+        public static readonly string IOS = "IOS";
+    }
+}
---
+++ Ratings/Enums/RatingMoodEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Ratings.Enums
+{
+    public static class RatingMoodEnum
+    {
+        public static readonly string LIKED = "LIKED";
+        public static readonly string NOT_LIKED = "NOT_LIKED";
+        public static readonly string INDIFFERENT = "INDIFFERENT";
+    }
+}
