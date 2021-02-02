fcb22ed81576aaadf152afe46076cada19572d75
Danilo Alves <danilo.alves@kinvo.com.br>
2021-02-02 12:35:00 -0300

    feat(DTO): Implements UserDTO

---
+++ Ratings/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Ratings.DTO
+{
+    public abstract class BaseUserDTO
+    {
+        public string Name { get; set; }
+    }
+
+    public class CreateUserDTO : BaseUserDTO { }
+
+    public class ReadUserDTO : BaseUserDTO
+    {
+        public int Id { get; set; }
+    }
+}
