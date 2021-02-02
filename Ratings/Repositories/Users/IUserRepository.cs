97a66e10d4d997c3c688510f4842863ee3a19faf
Danilo Alves <danilo.alves@kinvo.com.br>
2021-02-02 11:14:56 -0300

    feat(Repositories): Implements UserRepository

---
+++ Ratings/Repositories/Users/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ratings.Models;
+using System.Collections.Generic;
+
+namespace Ratings.Repositories.Users
+{
+    public interface IUserRepository
+    {
+        bool SaveChanges();
+
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        int Create(User user);
+        void Delete(int id);
+    }
+}
---
+++ Ratings/Repositories/Users/UserRepository.cs
@@ -0,0 +1,62 @@
+﻿using Ratings.Data;
+using Ratings.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ratings.Repositories.Users
+{
+    public class UserRepository : IUserRepository
+    {
+        private UsersContext _context;
+
+        public UserRepository(UsersContext context)
+        {
+            _context = context;
+        }
+
+        public int Create(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+           _context.Users.Add(user);
+
+            this.SaveChanges();
+
+            return user.Id;
+        }
+
+        public User GetById(int id)
+        {
+            var user = _context.Users.SingleOrDefault(user => user.Id == id);
+
+            return user;
+        }
+
+        public void Delete(int id)
+        {
+            var user = this.GetById(id);
+
+            _context.Users.Remove(user);
+
+            this.SaveChanges();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        public bool SaveChanges()
+        {
+            int totalRowsAffected = _context.SaveChanges();
+
+            return totalRowsAffected > 0;
+        }
+    }
+}
