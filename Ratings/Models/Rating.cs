7788263e06c98e118bb881ef34e2f2ee8376af45
Danilo Alves <danilo.alves@kinvo.com.br>
2021-02-02 10:55:11 -0300

    feat(Models): Implements Rating and User models

---
+++ Ratings/Models/Rating.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ratings.Models
+{
+    public class Rating
+    {
+        public Rating(int userId, string mood, string platform, string appVersion, string feedback)
+        {
+            this.UserId = userId;
+            this.Mood = mood;
+            this.Platform = platform;
+            this.AppVersion = appVersion;
+            this.Feedback = feedback;
+            this.ReviewDate = DateTime.Now;
+        }
+
+        public Rating(int userId, string mood, string platform, string appVersion) : this(userId, mood, platform, appVersion, null) { }
+
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        [Required]
+        public string Mood { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        public string AppVersion { get; set; }
+
+        public string Feedback { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+
+    }
+}
---
+++ Ratings/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ratings.Models
+{
+    public class User
+    {
+        public User(string name, IEnumerable<Rating> ratings)
+        {
+            this.Name = name;
+            this.Ratings = ratings;
+        }
+
+        public User(string name) : this(name, new List<Rating> { }) { }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public IEnumerable<Rating> Ratings { get; set; }
+    }
+}
